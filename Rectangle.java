6c79ea7828d355c0095d5a8d395c630e3504fef6
Hardik70 <148519007+rambos118@users.noreply.github.com>
2023-12-11 12:27:00 +0530

    Add files via upload

--- Rectangle.java
+++ Rectangle.java
@@ -15,11 +15,18 @@ public class Rectangle{
         this.breadth = breadth;
     }
 
+
+    public float calculatePerimeter(){
+        return 2*(length*breadth);
+    }
+
     public static void main(String[] args){
         Rectangle r = new Rectangle();
         r.setLength(5);
         r.setBreadth(3);
+        r.calculatePerimeter();
 
+        System.out.println("Calculate perimeter:"+ r.calculatePerimeter());
         System.out.println("Rectangles Length is " + r.length + " and breadth is " + r.breadth);
     }
 
