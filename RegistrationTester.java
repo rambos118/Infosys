1dafeb34ff60c816b10fa50fdb6cb23d1d6d7e7c
Hardik70 <148519007+rambos118@users.noreply.github.com>
2023-12-21 15:59:11 +0530

    Add files via upload

--- RegistrationTester.java
+++ RegistrationTester.java
@@ -1,74 +1,30 @@
-class Registration {
-    String customerName;
-    String passportNumber;
-    String licenseNumber;
-    String panCardNumber;
-    String voterId;
-    String[] phoneNumbers;
-
-    // Constructor for Passport and Phone Numbers
-    public Registration(String customerName, String passportNumber, String phoneNumber1, String phoneNumber2) {
-        this.customerName = customerName;
-        this.passportNumber = passportNumber;
-        this.phoneNumbers = new String[]{phoneNumber1, phoneNumber2};
-    }
-
-    // Constructor for License, Pan Card Numbers, and Phone Numbers
-    public Registration(String customerName, String licenseNumber, String panCardNumber, String phoneNumber1, String phoneNumber2) {
-        this.customerName = customerName;
-        this.licenseNumber = licenseNumber;
-        this.panCardNumber = panCardNumber;
-        this.phoneNumbers = new String[]{phoneNumber1, phoneNumber2};
-    }
-
-    // Constructor for Voter Id, License Number, and Phone Numbers
-    public Registration(String customerName, String voterId, String licenseNumber, String panCardNumber, String phoneNumber1, String phoneNumber2) {
-        this.customerName = customerName;
-        this.voterId = voterId;
-        this.licenseNumber = licenseNumber;
-        this.panCardNumber = panCardNumber;
-        this.phoneNumbers = new String[]{phoneNumber1, phoneNumber2};
-    }
-
-    // Display registration details
-    public void displayRegistrationDetails() {
-        System.out.println("Congratulations " + customerName + "!!! you have been successfully registered for our services with the following details:");
-
-        if (passportNumber != null) {
-            System.out.println("Passport number: " + passportNumber);
-        } else {
-            System.out.println("License number: " + licenseNumber);
-            System.out.println("Pan card number: " + panCardNumber);
-        }
-
-        if (voterId != null) {
-            System.out.println("Voter id: " + voterId);
-        }
-
-        System.out.println("Phone numbers: " + phoneNumbers[0]);
-        System.out.println(phoneNumbers[1]);
-    }
-}
-
 public class RegistrationTester {
     public static void main(String[] args) {
-        // Test cases
-        Registration user1 = new Registration("Kevin", "MN9891N", "9452425421", "7676765252");
-        user1.displayRegistrationDetails();
+        // Create two student registration objects
+        StudentRegistration student1 = new StudentRegistration("Peter", 23, 5001);
+        StudentRegistration student2 = new StudentRegistration("Peter", 24, 5003);
 
-        System.out.println(); // Add a newline for better readability
+        // Generate roll number for both students
+        student1.generateRollNumber();
+        student2.generateRollNumber();
 
-        Registration user2 = new Registration("Julias", "123", "PN7878", "2345615451", "6763562562");
-        user2.displayRegistrationDetails();
+        // Display student details
+        displayStudentDetails(student1);
+        displayStudentDetails(student2);
 
-        System.out.println(); // Add a newline for better readability
+        // Compare objects and display the result
+        if (student1.equals(student2)) {
+            System.out.println("Objects are equal.");
+        } else {
+            System.out.println("Objects are not equal.");
+        }
+    }
 
-        Registration user3 = new Registration("Jammy", "45453", "765", "9634524353", "9887373737");
-        user3.displayRegistrationDetails();
-
-        System.out.println(); // Add a newline for better readability
-
-        Registration user4 = new Registration("Rose", "PN8934", "34356", "9867456367", "7645367356");
-        user4.displayRegistrationDetails();
+    private static void displayStudentDetails(StudentRegistration student) {
+        System.out.println("Student Details");
+        System.out.println("Name: " + student.getStudentName());
+        System.out.println("Admission Number: " + student.getAdmissionNumber());
+        System.out.println("Roll Number: " + student.getRollNumber());
+        System.out.println();
     }
 }
